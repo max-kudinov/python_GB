ab47f9ee5d2a7ef4c27f90dd6066018af62e92d2
mkudinov <m.kudinov@icloud.com>
2023-09-22 13:51:53 +0300

    fixed formatting

--- task_36.py
+++ task_36.py
@@ -1,7 +1,7 @@
 def print_operation_table(operation, num_rows=6, num_columns=6):
     for i in range(1, num_rows + 1):
         for j in range(1, num_columns + 1):
-            print(operation(i, j), end=" ")
+            print(operation(i, j), end="\t")
         print()
 
 
